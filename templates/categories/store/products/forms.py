f21a9c976eb046a17c06d253690809d3d608ee6c
Ivan103115 <ivanshinkarev312@gmail.com>
2023-02-03 18:16:34 +0300

    'first'

--- templates/categories/store/products/forms.py
+++ templates/categories/store/products/forms.py
@@ -2,11 +2,11 @@ from django import forms
 
 
 class ProductCreateForm(forms.Form):
-    title = forms.CharField(min_length=100)
+    title = forms.CharField(max_length=100)
     description = forms.CharField(widget=forms.Textarea())
     price = forms.FloatField(required=False)
     rate = forms.FloatField(required=False)
 
 
-class CommentCreateForm(forms.Form):
+class ReviewCreateForm(forms.Form):
     title = forms.CharField()
--- templates/categories/store/products/views.py
+++ templates/categories/store/products/views.py
@@ -1,71 +1,59 @@
 from django.shortcuts import render, redirect
-from products.models import Product, Comment, Category
-from products.forms import ProductCreateForm, CommentCreateForm
+from products.models import Product, Review, Category
+from products.forms import ProductCreateForm, ReviewCreateForm
 
 
 # Create your views here.
-PAGINATION_LIMIT = 3
 
 
 def main(request):
     if request.method == 'GET':
-        return render(request, 'layosts/index.html')
+        return render(request, 'layouts/index.html')
+
 
 def products_view(request):
     if request.method == 'GET':
-        products = Product.objects.all()
         category_id = request.GET.get('category')
-        search = request.GET.get('search')
-        page = int(request.GET.get('page', 1))
-        if search is not None:
-            products = Product.objects.filter(
-                title__icontains=search
-            )
-
-        max_page = products.__len__() / PAGINATION_LIMIT
-        if round(max_page) < max_page:
-            max_page = round(max_page) + 1
-        else:
-            max_page = round(max_page)
-
-        products = products[PAGINATION_LIMIT * (page - 1):PAGINATION_LIMIT * page]
-
         if category_id:
             products = Product.objects.filter(category=Category.objects.get(id=category_id))
-
+        else:
+            products = Product.objects.all()
         context = {
             'products': products,
-            'user': request.user,
-            'max_page': range(1, max_page+1)
+            'user': request.user
         }
         return render(request, 'products/products.html', context=context)
 
+
 def main_view(request):
     if request.method == 'GET':
-        return render(request, 'layosts/index.html')
+        return render(request, 'layouts/index.html')
 
 
 def product_detail_view(request, id):
     if request.method == 'GET':
         product_obj = Product.objects.get(id=id)
-        reviews = Comment.objects.filter(product=product_obj)
+        reviews = Review.objects.filter(product=product_obj)
 
         context = {
             'product': product_obj,
             'reviews': reviews,
-            'form': CommentCreateForm
+            'form': ReviewCreateForm,
         }
         return render(request, 'products/detail.html', context=context)
     if request.method == 'POST':
         product_obj = Product.objects.get(id=id)
-        reviews = Comment.objects.filter(product=product_obj)
-        form = CommentCreateForm(data=request.POST)
-        if form.is_valid():
-            Comment.objects.create(
+        reviews = Review.objects.filter(product=product_obj)
+        form = ReviewCreateForm(data=request.POST)
+        if form.is_valid() and not request.user.is_anonymous:
+            Review.objects.create(
+                author_id=request.user.id,
                 title=form.cleaned_data.get('title'),
                 product=product_obj
             )
             return redirect(f'/products/{product_obj.id}/')
+        else:
+            form.add_error('title', 'ti ne zaregan')
         return render(request, 'products/detail.html', context={
             'product': product_obj,
             'reviews': reviews,
@@ -85,11 +73,13 @@ def categories_view(request):
 
 
 def create_product_view(request):
-    if request.method == 'GET':
+    if request.method == 'GET' and not request.user.is_anonymous:
         context = {
             'form': ProductCreateForm
         }
         return render(request, 'products/create.html', context=context)
+    elif request.user.is_anonymous:
+        return redirect('/products')
 
     if request.method == 'POST':
         form = ProductCreateForm(data=request.POST)
