f3ef1b36288a346c5500b96d207d000a474f451d
Ivan103115 <ivanshinkarev312@gmail.com>
2023-02-07 16:07:58 +0300

    'hw8'

--- products/views.py
+++ products/views.py
@@ -1,107 +1,72 @@
 from django.shortcuts import render, redirect
-from products.models import Product, Comment, Category
-from products.forms import ProductCreateForm, CommentCreateForm
+from django.contrib.auth import authenticate, logout, login
+from users.forms import LoginForm, RegisterForm
+from django.contrib.auth.models import User
+from django.views.generic import ListView, RedirectView, CreateView
 
 
-# Create your views here.
-PAGINATION_LIMIT = 3
 
+class LoginView(ListView, CreateView):
+    template_name = 'users/login.html'
 
-def main(request):
-    if request.method == 'GET':
-        return render(request, 'layosts/index.html')
-
-def products_view(request):
-    if request.method == 'GET':
-        products = Product.objects.all()
-        category_id = request.GET.get('category')
-        search = request.GET.get('search')
-        page = int(request.GET.get('page', 1))
-        if search is not None:
-            products = Product.objects.filter(
-                title__icontains=search
-            )
-
-        max_page = products.__len__() / PAGINATION_LIMIT
-        if round(max_page) < max_page:
-            max_page = round(max_page) + 1
-        else:
-            max_page = round(max_page)
-
-        products = products[PAGINATION_LIMIT * (page - 1):PAGINATION_LIMIT * page]
-
-        if category_id:
-            products = Product.objects.filter(category=Category.objects.get(id=category_id))
-
+    def get(self, request, **kwargs):
         context = {
-            'products': products,
-            'user': request.user,
-            'max_page': range(1, max_page+1)
+            'form': LoginForm
         }
-        return render(request, 'products/products.html', context=context)
+        return render(request, self.template_name, context=context)
 
-def main_view(request):
-    if request.method == 'GET':
-        return render(request, 'layosts/index.html')
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(data=request.POST)
 
-
-def product_detail_view(request, id):
-    if request.method == 'GET':
-        product_obj = Product.objects.get(id=id)
-        reviews = Comment.objects.filter(product=product_obj)
-
-        context = {
-            'product': product_obj,
-            'reviews': reviews,
-            'form': CommentCreateForm
-        }
-        return render(request, 'products/detail.html', context=context)
-    if request.method == 'POST':
-        product_obj = Product.objects.get(id=id)
-        reviews = Comment.objects.filter(product=product_obj)
-        form = CommentCreateForm(data=request.POST)
         if form.is_valid():
-            Comment.objects.create(
-                title=form.cleaned_data.get('title'),
-                product=product_obj
+            user = authenticate(
+                username=form.cleaned_data.get('username'),
+                password=form.cleaned_data.get('password')
             )
-            return redirect(f'/products/{product_obj.id}/')
-        return render(request, 'products/detail.html', context={
-            'product': product_obj,
-            'reviews': reviews,
+            if user:
+                login(request, user)
+                return redirect('/products')
+            else:
+                form.add_error('username', 'Неправильный логин или пароль')
+
+        return render(request, 'users/login.html', context={
             'form': form
         })
 
 
 
-def categories_view(request):
-    if request.method == 'GET':
-        categories = Category.objects.all()
+
+class LogoutView(RedirectView):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect('/products/')
+
+
+
+
+class RegisterView(ListView, CreateView):
+    template_name = 'users/register.html'
+
+    def get(self, request, *args, **kwargs):
         context = {
-            'categories': categories,
+            'form': RegisterForm
         }
+        return render(request, 'users/register.html', context=context)
 
-        return render(request, 'categories/index.html', context=context)
-
-
-def create_product_view(request):
-    if request.method == 'GET':
-        context = {
-            'form': ProductCreateForm
-        }
-        return render(request, 'products/create.html', context=context)
-
-    if request.method == 'POST':
-        form = ProductCreateForm(data=request.POST)
+    def post(self, request, *args, **kwargs):
+        form = RegisterForm(data=request.POST)
 
         if form.is_valid():
-            Product.objects.create(
-                title=form.cleaned_data.get('title'),
-                description=form.cleaned_data.get('description'),
-                price=form.cleaned_data['price'] if form.cleaned_data['price'] is not None else 0,
-                rate=form.cleaned_data['rate'] if form.cleaned_data['rate'] is not None else 5,
-            )
-            return redirect('/products/')
-        return render(request, 'products/create.html', context={
+            password1, password2 = form.cleaned_data.get('password1'), form.cleaned_data.get('password2')
+            if password1 == password2:
+                User.objects.create_user(
+                    username=form.cleaned_data.get('username'),
+                    password=form.cleaned_data.get('password1')
+                )
+                return redirect('/users/login/')
+            else:
+                form.add_error('password1', 'Paroli ne sovpadayut')
+
+        return render(request, 'users/register.html', context={
             'form': form
         })
--- store/forms.py
+++ store/forms.py
@@ -2,11 +2,12 @@ from django import forms
 
 
 class ProductCreateForm(forms.Form):
-    title = forms.CharField(min_length=100)
+    image = forms.FileField()
+    title = forms.CharField(max_length=100)
     description = forms.CharField(widget=forms.Textarea())
     price = forms.FloatField(required=False)
     rate = forms.FloatField(required=False)
 
 
-class CommentCreateForm(forms.Form):
+class ReviewCreateForm(forms.Form):
     title = forms.CharField()
